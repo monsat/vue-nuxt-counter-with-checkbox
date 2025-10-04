f75a8322f358dc834e408ec7cca27c1ccd61dc81
mon_sat <tnk@daijiten.info>
2025-10-04 12:24:37 +0900

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const checked = ref(false)
+</script>
+
+<template>
+  <div>
+    <AppCheckBox v-model="checked" />
+    <AppCounter v-if="checked" />
+    <AppCounter v-if="!checked" />
+  </div>
+</template>
---
+++ app/components/AppCheckBox.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const checked = defineModel<boolean>({
+  default: false,
+})
+</script>
+
+<template>
+  <input
+    v-model="checked"
+    type="checkbox"
+    @click="checked = !checked"
+  />
+</template>
